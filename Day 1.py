e57b88a91bd5d73f8898e8eb562705175a275ad6
Grace Mwaura <gracemwaura1404@gmail.com>
2024-03-11 23:57:41 +0300

    Day 1

---
+++ Day 1.py
@@ -0,0 +1,5 @@
+print("Hello there?")
+city = (input("What is the name of the city you grew up in? \n "))
+pet = (input("What is the name of your favorite pet \n"))
+bandname = city + pet
+print("Your bandname is " + bandname)
